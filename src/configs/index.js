8042b75a44af59e65421386b6f5c2d3d7e64b5f1
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-01-24 21:06:52 -0300

    change route

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,6 +2,6 @@ module.exports = {
   db_connection:
     "mongodb://palmieriunifor02:03102190@mongo71-farm10.kinghost.net:27017/palmieriunifor02",
   mlab: "mongodb://palmieri:natan210290@ds061681.mlab.com:61681/palmieri",
-  photo_url: "http://palmieriuniformes.nodejs7602.kinghost.net:21045/img",
+  photo_url: "http://palmieriuniformes.nodejsng36f02.kinghost.net:21045/img",
   //photo_url: "http://localhost:21489/img",
 };
